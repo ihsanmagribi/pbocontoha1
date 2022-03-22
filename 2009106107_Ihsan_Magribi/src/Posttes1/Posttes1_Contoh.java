5d8cd79c5c3460105f189d7c6d9e1caf100dafd8
Ihsan Magribi <ihsanmahgribi@gmail.com>
2022-03-23 02:56:43 +0800

    contoh1

---
+++ 2009106107_Ihsan_Magribi/src/Posttes1/Posttes1_Contoh.java
@@ -0,0 +1,8 @@
+
+package Posttes1;
+public class Posttes1_Contoh {
+    public static void main(String[] args) {
+        System.out.println("Coba-Coba ygy\nIHSAN MAGRIBI_2009106107");
+    }
+    
+}
